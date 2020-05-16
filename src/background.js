766ed5d43ce9882ac34b8b525ae095e4953e86d3
Viet Hoang <ngviethoang0212@gmail.com>
2020-05-16 11:48:01 +0700

    first init

---
+++ src/background.js
@@ -0,0 +1,20 @@
+searchUrbanDict = function(word) {
+    var query = word.selectionText;
+    chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + query});
+};
+
+goToIncognito = function(page) {
+    chrome.windows.create({url: page.pageUrl, incognito: true})
+}
+  
+chrome.contextMenus.create({
+    title: "Search in UrbanDictionary",
+    contexts:["selection"],
+    onclick: searchUrbanDict
+});
+
+chrome.contextMenus.create({
+    title: "Incognito this Tab",
+    contexts:["page"],
+    onclick: goToIncognito
+});
