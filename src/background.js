cb38304a50de8cf3c130ccb142fabd8915a9ff40
Viet Hoang <ngviethoang0212@gmail.com>
2021-06-17 21:31:31 +0700

    update

--- src/background.js
+++ src/background.js
@@ -12,56 +12,84 @@ chrome.contextMenus.create({
     contexts:["selection"],
     onclick: function(word) {
         const query = word.selectionText
-        chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + query})
+        chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + query});
     }
-})
+});
 
 chrome.contextMenus.create({
     title: "Incognito this Tab",
     contexts:["page"],
     onclick: function(page) {
-        chrome.windows.create({url: page.pageUrl, incognito: true})
+        chrome.windows.create({url: page.pageUrl, incognito: true});
     }
-})
+});
 
 chrome.contextMenus.create({
     title: "Save to Dashboard",
     contexts:["page", "image"],
-    onclick: function(info, tab) {
+    onclick: async (info, tab) => {
         let data = {
             name: tab['title'],
             source: info['pageUrl'],
-        }
+        };
         if (info['mediaType'] === 'image') {
-            data['image'] = info['srcUrl']
+            data['image'] = info['srcUrl'];
         }
-        fetch(`${API_URL_PREFIX}dashboard/cards`, {
+        let response = await fetch(`${API_URL_PREFIX}dashboard/cards`, {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': API_ACCESS_TOKEN,
             }
-        }).then(response => {
-            response.json().then(response => {
-                const databaseId = response.databaseId.replaceAll('-', '')
-                const pageId = response.id.replaceAll('-', '')
-                const url = `${NOTION_BASE_URL}${databaseId}?p=${pageId}`
-                chrome.tabs.create({url})
-            })
-        })
+        });
+        response = await response.json();
+        const databaseId = response.databaseId.replaceAll('-', '');
+        const pageId = response.id.replaceAll('-', '');
+        const url = `${NOTION_BASE_URL}${databaseId}?p=${pageId}`;
+        chrome.tabs.create({url});
     }
-})
+});
+
+chrome.contextMenus.create({
+    title: 'Focus Mode',
+    contexts: ['all'],
+    onclick: async (info, tab) => {
+        let response = await fetch(`${API_URL_PREFIX}flashcard/settings`);
+        response = await response.json();
+        let focusMode = false;
+        const focusModeItem = (response || []).find(item => item.Key === 'focusMode');
+        if (focusModeItem) {
+            focusMode = focusModeItem.Value == 1;
+        }
+        
+        const message = focusMode ? 'Disable?' : 'Enable?';
+        if (confirm(message)) {
+            const data = {
+                key: 'focusMode',
+                value: focusMode ? '0' : '1',
+            };
+            fetch(`${API_URL_PREFIX}flashcard/settings`, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': API_ACCESS_TOKEN,
+                }
+            });
+        }
+    }
+});
 
 chrome.contextMenus.create({
     type: 'separator',
     contexts: ['all'],
-})
+});
 
 chrome.contextMenus.create({
     title: "Inspect",
     contexts:["all"],
     onclick: function(info, tab) {
-        alert(JSON.stringify(info) + '\n\n', JSON.stringify(tab))
+        alert(JSON.stringify(info) + '\n\n', JSON.stringify(tab));
     }
-})
+});
--- src/content.js
+++ src/content.js
@@ -1,14 +1,27 @@
+
+const API_URL_PREFIX = 'https://vh-tracker.herokuapp.com/api/'
+
+const currentUrl = window.location.href;
+
 const BLOCK_KEYWORDS = [
     
-]
+];
 
-let current = window.location.href
-for (let blockKeyword of BLOCK_KEYWORDS) {
-    if(current.includes(blockKeyword)) {
-        document.documentElement.innerHTML = ''
-        document.documentElement.innerHTML = 'Domain is blocked'
-        document.documentElement.scrollTop = 0
-        break
+(async () => {
+    for (const blockKeyword of BLOCK_KEYWORDS) {
+        if(currentUrl.includes(blockKeyword)) {
+            let response = await fetch(`${API_URL_PREFIX}flashcard/settings`);
+            response = await response.json();
+            let focusMode = false;
+            const focusModeItem = (response || []).find(item => item.Key === 'focusMode');
+            if (focusModeItem) {
+                focusMode = focusModeItem.Value == 1;
+            }
+            if (focusMode) {
+                window.location.href = 'http://localhost:8080';
+            }
+            break;
+        }
     }
-}
+})();
   
