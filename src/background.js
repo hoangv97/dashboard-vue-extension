a9bd6e3356bb3a657d4ed770ec0fdf188bb8d2ab
Viet Hoang <ngviethoang0212@gmail.com>
2022-09-07 17:01:40 +0700

    update selection context menu

--- src/background.js
+++ src/background.js
@@ -1,3 +1,43 @@
+// helpers
+function notionCall(path, method, body) {
+  fetch(
+    `https://mysrs.netlify.app/.netlify/functions/cors?url=https://api.notion.com/v1/${path}`,
+    {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Notion-Version': '2022-06-28',
+        Authorization: `Bearer ${process.env.NOTION_DASHBOARD_TOKEN}`,
+      },
+      body: JSON.stringify(body),
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      alert('Success');
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+function notionAppendBlockChildren(blockId, children) {
+  notionCall(`blocks/${blockId}/children`, 'PATCH', { children });
+}
+
+function notionCreatePage(parent, properties, children) {
+  notionCall(`pages`, 'POST', {
+    parent,
+    properties,
+    children,
+  });
+}
+
+const truncate = (input, length) =>
+  input.length > length ? `${input.substring(0, length)}...` : input;
+
 // https://developer.chrome.com/docs/extensions/reference/contextMenus/
 
 chrome.contextMenus.create({
@@ -24,35 +64,52 @@ chrome.contextMenus.create({
   contexts: ['page'],
   onclick: function(page) {
     const url = new URL(page.pageUrl);
-    fetch(
-      `https://mysrs.netlify.app/.netlify/functions/cors?url=https://api.notion.com/v1/blocks/${process.env.NOTION_READING_ID}/children`,
+    notionAppendBlockChildren(process.env.NOTION_READING_ID, [
       {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          'Notion-Version': '2022-06-28',
-          Authorization: `Bearer ${process.env.NOTION_DASHBOARD_TOKEN}`,
-        },
-        body: JSON.stringify({
-          children: [
-            {
-              object: 'block',
-              type: 'bookmark',
-              bookmark: { url },
+        object: 'block',
+        type: 'bookmark',
+        bookmark: { url },
+      },
+    ]);
+  },
+});
+
+chrome.contextMenus.create({
+  title: 'Save to Notion Reading - Page',
+  contexts: ['selection'],
+  onclick: function(info, tab) {
+    const text = info.selectionText;
+    alert(text.split('\n'));
+    notionCreatePage(
+      {
+        type: 'page_id',
+        page_id: process.env.NOTION_READING_ID,
+      },
+      {
+        title: [
+          {
+            text: {
+              content: truncate(text, 50),
             },
-          ],
-        }),
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        alert('Success');
-      })
-      .catch((error) => {
-        alert(error);
-      });
+          },
+        ],
+      },
+      [
+        {
+          type: 'paragraph',
+          paragraph: {
+            rich_text: [
+              {
+                type: 'text',
+                text: {
+                  content: text,
+                },
+              },
+            ],
+          },
+        },
+      ]
+    );
   },
 });
 
